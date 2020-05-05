35f77e11bbe5f92ef686ec0133ae7e0b3cbd9602
cezarmathe <me@cezarmathe.com>
2020-05-05 18:20:20 +0300

    ci: Fix travis build

--- build.rs
+++ build.rs
@@ -52,7 +52,7 @@ fn build() {
 }
 
 fn main() {
-    if cfg!(not(docs_rs)) {
+    if cfg!(not(docs_rs)) && cfg!(not(travis_ci)) {
         println!("cargo:rustc-link-lib=btrfsutil");
     }
     build();
